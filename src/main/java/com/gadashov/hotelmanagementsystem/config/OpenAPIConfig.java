c907f361c540c5111235776fb60ddae20bf6ae7b
Ali Gadashov <gadashovali13@gmail.com>
2024-03-01 09:47:03 +0400

    GlobalExceptionHandler updated

--- src/main/java/com/gadashov/hotelmanagementsystem/config/OpenAPIConfig.java
+++ src/main/java/com/gadashov/hotelmanagementsystem/config/OpenAPIConfig.java
@@ -1,12 +1,9 @@
 package com.gadashov.hotelmanagementsystem.config;
 
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
  */
 
 @Configuration
-public class SwaggerConfig {
+public class OpenAPIConfig {
 
     @Bean
     public OpenAPI openAPI() {
--- src/main/java/com/gadashov/hotelmanagementsystem/handler/GlobalExceptionHandler.java
+++ src/main/java/com/gadashov/hotelmanagementsystem/handler/GlobalExceptionHandler.java
@@ -1,12 +1,20 @@
 package com.gadashov.hotelmanagementsystem.handler;
 
-import com.gadashov.hotelmanagementsystem.model.dto.response.ExceptionResponse;
+import com.gadashov.hotelmanagementsystem.model.enums.Exceptions;
 import com.gadashov.hotelmanagementsystem.model.exceptions.GlobalException;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Author: Ali Gadashov
@@ -14,11 +22,42 @@ import java.util.List;
  */
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends DefaultErrorAttributes {
     @ExceptionHandler(GlobalException.class)
-    public ResponseEntity<ExceptionResponse> handle(GlobalException exception){
-        List<String> message = List.of(exception.getExceptions().getMessage());
-        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getExceptions().getStatus().value(),message);
-        return ResponseEntity.status(exception.getExceptions().getStatus()).body(exceptionResponse);
+    public ResponseEntity<Map<String, Object>> handle(GlobalException ex, WebRequest request){
+        Map<String, Object> errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+
+        Exceptions exceptions = ex.getExceptions();
+
+        errorAttributes.put("message", exceptions.getMessage());
+        errorAttributes.put("status",exceptions.getStatus());
+        errorAttributes.put("path", ((ServletWebRequest)request).getRequest().getRequestURI());
+        errorAttributes.put("timestamp", LocalDateTime.now());
+
+        return ResponseEntity
+                .status(exceptions.getStatus())
+                .body(errorAttributes);
     }
+
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<Map<String, Object>> handle(MethodArgumentNotValidException ex, WebRequest request){
+        Map<String, Object> errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        Map<String, Object> invalidFields = new HashMap<>();
+
+        ex.getFieldErrors()
+                .forEach(fieldError -> invalidFields.put(fieldError.getField(), fieldError.getDefaultMessage()));
+
+        errorAttributes.put("error", invalidFields);
+        errorAttributes.put("status", HttpStatus.BAD_REQUEST.value());
+        errorAttributes.put("path", ((ServletWebRequest)request).getRequest().getRequestURI());
+        errorAttributes.put("timestamp", LocalDateTime.now());
+
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorAttributes);
+    }
+
+
+
 }
